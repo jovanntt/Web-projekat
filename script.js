677b4f9d857e6a363cd365844a791f9cc83203b0
jovanntt <156968145+jovanntt@users.noreply.github.com>
2024-01-20 08:21:21 +0100

    Finalni projekat

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('scroll', handleScroll);
+
+function handleScroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;  
+    var scrolled = (winScroll / height) * 100;
+  
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+var myVar = setInterval(function() {
+    myTimer();
+  }, 1000);
+  
+  function myTimer() {
+    var d = new Date();
+    document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+  }
